a3eff7a538f2000ecab7f72f99ca7a11ea517690
cesarc00 <cesarc00@yahoo.com>
2013-04-12 12:34:30 +0200

    twi

---
+++ src/com/ejemplo/twitter/Usuario.java
@@ -0,0 +1,21 @@
+package com.ejemplo.twitter;
+
+public class Usuario {
+
+	private int id;
+	private String nombre;
+	private String email;
+	private String contraseña;
+	private String userName;
+	private String bio;
+	private String foto;
+	private String fotoEncabezado;
+	private String web;
+	private String ubicacion, pais;
+	private String zonaHorario, idioma;
+	
+	// poner todas otras en una tabla (y objeto) aparte, ver clase opciones
+
+	
+	
+}
---
+++ src/com/ejemplo/twitter/opcionesUsuario.java
@@ -0,0 +1,16 @@
+package com.ejemplo.twitter;
+
+import java.util.ArrayList;
+
+public class opcionesUsuario {
+
+	private boolean syncFace; // --> esto se haría con un token que tenga idUserFace, pwdFace y sync
+	private boolean visibilidadBusquedasEmail;
+	private String ubicacionTweet;
+	private boolean protegerTweets;
+	private boolean multimediaSensible;
+	private boolean mostrarSensible;
+	private ArrayList<String> preguntasContraseña;
+	
+	
+}
