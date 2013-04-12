95ecd3fed8fb20b9ae3609222c1a8902f722fadf
cesarc00 <cesarc00@yahoo.com>
2013-04-12 12:58:19 +0200

    t

---
+++ src/com/ejemplo/twitter/Tweet.java
@@ -0,0 +1,13 @@
+package com.ejemplo.twitter;
+
+import java.util.ArrayList;
+
+public class Tweet {
+
+	private int id;
+	private Usuario autor;
+	private String fecha;
+	private String texto;
+	private  ArrayList<Usuario> listaRt; // Con .size tendremos el total y dentro tenemos la lista users.
+	private ArrayList<Usuario> listaFv;
+}
